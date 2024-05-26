9f49b11127165ff1578054d25123f05c6c88a04e
Coder Shaira <kangiakashish2001@gmail.com>
2024-05-26 15:37:24 +0530

    Initial commit

---
+++ src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,37 @@
+package com.luv2code.ecommerce.entity;
+
+import java.util.Set;
+
+import org.hibernate.annotations.Cascade;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="product_category")
+// @Data -- known bug
+@Getter
+@Setter
+public class ProductCategory {
+	
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "category_name")
+	private String categoryName;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy ="category")
+	private Set<Product> products;
+}
